e3d7102ea2618e305621c1e2104754d9a7898514
Wendel Lopes <wendelopes@msn.com>
2020-04-12 18:15:16 -0300

    03 - Criação e configuração do model

---
+++ app/models/article.rb
@@ -0,0 +1,6 @@
+class Article < ApplicationRecord
+
+  validates :title, presence: true, length: {minimum: 5, maximum: 20}
+  validates :description, presence: true, length: {minimum: 10, maximum: 300}
+
+end
